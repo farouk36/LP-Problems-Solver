bcf3a671171806084492748a0c1a901270b8053f
farouk36 <faroukashraf36@gmail.com>
2025-03-23 07:31:40 +0200

    handle 2 phase iterations

--- GUI/print_two_phase.py
+++ GUI/print_two_phase.py
@@ -64,81 +64,80 @@ def print_two_phase_iterations(solution, iterations, main_row, basic_vars):
 
         html+=print_tableau(first_phase[0],main_row,basic_vars,'Z',"Initial Tableau With Z")
         html += print_tableau(first_phase[1], main_row, basic_vars, 'r', "Start by Minimize r ")
-
-        tableau_iterations = [it for it in first_phase if isinstance(it, np.ndarray)]
-        entering_leaving_var = [it for it in first_phase if not isinstance(it, np.ndarray)]
-        j = 0
-
-        for i, tableau in enumerate(tableau_iterations[start_index_first_phase:], start_index_first_phase):
-            html += f"""
-            <h3 style="color: #88C0D0;">Iteration {i-start_index_first_phase+1}</h3>
-            <hr style="border-color: #4C566A;">
-            """
-
-            if i > start_index_first_phase and i !=len(tableau_iterations) :
-                entering = entering_leaving_var[j][0]
-                leaving = entering_leaving_var[j][1]
-                j=j+1
-                print(entering)
-                print(leaving)
-
-                if leaving in basic_vars:
-                    basic_vars[basic_vars.index(leaving)] = entering
-
-                html += f"""
-                <p><b>Entering Variable:</b> {entering}</p>
-                <p><b>Leaving Variable:</b> {leaving}</p>
-                """
-
-            html += f"""
-            <p><b>Basic Variables:</b> {', '.join(basic_vars)}</p>
-            <p><b>Tableau:</b></p>
-            <table border="1" cellpadding="5" style="border-collapse: collapse; border-color: #4C566A;">
-                <tr style="background-color: #4C566A;">
-                    <th></th>
-            """
-
-            for var in main_row:
-                html += f"<th>{var}</th>"
-            html += "<th>Solution</th></tr>"
-
-            html += """
-            <tr>
-                <td>r</td>
-            """
-            for val in tableau[-1]:
-                if val.is_integer():
-                    html += f"<td>{int(val)}</td>"
-                else:
-                    html += f"<td>{val:.4f}</td>"
-            html += "</tr>"
-
-            for k, row in enumerate(tableau[:-1], 1):
-                html += """<tr style="background-color: #3B4252;">"""
-                html += f"<td>{basic_vars[k - 1]}</td>"
-
-                for val in row:
-                    if val.is_integer():
-                        html += f"<td>{int(val)}</td>"
-                    else:
-                        html += f"<td>{val:.4f}</td>"
-                html += "</tr>"
-
-            html += "</table><br>"
-
-            # Add a separator between iterations except for the last one
-            if i < len(tableau_iterations) - 1:
-                html += "<hr style='border-color: #4C566A; margin: 20px 0;'>"
+        html+=print_table(first_phase,basic_vars, main_row,start_index_first_phase,1)
 
         new_main_row=[item for item in main_row if not (isinstance(item, str) and item.startswith('a'))]
-        print(main_row)
         html += print_tableau(second_phase[0], new_main_row, basic_vars, 'Z', "Replace r with Z")
-        for i,tableau in enumerate(second_phase[1:],1):
-           print(i)
-           html += print_tableau(second_phase[i], new_main_row, basic_vars, 'Z', "Result")
+        html+=print_table(second_phase,basic_vars, new_main_row)
         html += """
         </body>
         </html>
         """
-
         return html
+
+def print_table(phase,basic_vars,main_row,start=1,is_first=0):
+     html = """"""
+     tableau_iterations = [it for it in phase if isinstance(it, np.ndarray)]
+     entering_leaving_var = [it for it in phase if not isinstance(it, np.ndarray)]
+     j = 0
+     for i, tableau in enumerate(tableau_iterations[start:], start):
+
+         html += f"""
+                <h3 style="color: #88C0D0;">Iteration {i-start+1}</h3>
+                <hr style="border-color: #4C566A;">
+                """
+
+         if ((is_first and i > start)or(not is_first and i>0)) and i != len(tableau_iterations) and j < len(entering_leaving_var):
+             entering = entering_leaving_var[j][0]
+             leaving = entering_leaving_var[j][1]
+             j = j + 1
+
+             if leaving in basic_vars:
+                 basic_vars[basic_vars.index(leaving)] = entering
+
+             html += f"""
+                    <p><b>Entering Variable:</b> {entering}</p>
+                    <p><b>Leaving Variable:</b> {leaving}</p>
+                    """
+
+         html += f"""
+                <p><b>Basic Variables:</b> {', '.join(basic_vars)}</p>
+                <p><b>Tableau:</b></p>
+                <table border="1" cellpadding="5" style="border-collapse: collapse; border-color: #4C566A;">
+                    <tr style="background-color: #4C566A;">
+                        <th></th>
+                """
+
+         for var in main_row:
+             html += f"<th>{var}</th>"
+         html += "<th>Solution</th></tr>"
+
+         html += """
+                <tr>
+                    <td>r</td>
+                """
+         for val in tableau[-1]:
+             if val.is_integer():
+                 html += f"<td>{int(val)}</td>"
+             else:
+                 html += f"<td>{val:.4f}</td>"
+         html += "</tr>"
+
+         for k, row in enumerate(tableau[:-1], 1):
+             html += """<tr style="background-color: #3B4252;">"""
+             html += f"<td>{basic_vars[k - 1]}</td>"
+
+             for val in row:
+                 if val.is_integer():
+                     html += f"<td>{int(val)}</td>"
+                 else:
+                     html += f"<td>{val:.4f}</td>"
+             html += "</tr>"
+
+         html += "</table><br>"
+
+         # Add a separator between iterations except for the last one
+         if i < len(tableau_iterations) - 1:
+             html += "<hr style='border-color: #4C566A; margin: 20px 0;'>"
+
+     return html
