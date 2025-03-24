13ff50e58b2e851e11b78fdad7900819d4481067
Abdelrahman-saeed <abdelrahmansaeed948@gmail.com>
2025-03-25 01:13:58 +0200

    handle urs vars at goal-Method and add report file

--- Goal_Programming.py
+++ Goal_Programming.py
@@ -1,4 +1,6 @@
 import numpy as np
+# from PyQt6.QtNetwork.QHttpHeaders import insert
+
 
 def __excute_simplex(tableau, basic_var, main_row, artificial_vars ,phase, isMax  , priorities):
     iterations = []
@@ -88,8 +90,8 @@ def __excute_simplex(tableau, basic_var, main_row, artificial_vars ,phase, isMax
     for i in range (len(basic_var), len(tableau)):
         if (all(tableau[i , :-1]<=0)):
             isDone[i-len(basic_var)] = True
-            
-        else : 
+
+        else :
             isDone[i-len(basic_var)] = False
     # Extract solution
     solution = {}
@@ -103,7 +105,11 @@ def __excute_simplex(tableau, basic_var, main_row, artificial_vars ,phase, isMax
     solution['objective_value'] = tableau[-1, -1]
 
     return iterations, solution, basic_var , isDone
+
+
 def make_vars_zeros_Linearly(tablue, main_row, basic_var):
+    print(main_row)
+    print(basic_var)
     index_in_basic = 0
     for i in range(len(main_row)):
         if main_row[i] in basic_var:
@@ -115,10 +121,10 @@ def make_vars_zeros_Linearly(tablue, main_row, basic_var):
 
 def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_types,variable_types , priorities):
 
-    
 
-    # # Handle unrestricted variables by splitting them into positive and negative parts
-    # unrestricted_indices = [i for i, v_type in enumerate(variable_types) if v_type == "Unrestricted"]
+
+    # Handle unrestricted variables by splitting them into positive and negative parts
+    unrestricted_indices = [i for i, v_type in enumerate(variable_types) if v_type == "Unrestricted"]
     # print(unrestricted_indices)
 
     # if unrestricted_indices:
@@ -151,8 +157,15 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
     num_slack=0
     mainRow = []
     basic_var = []
+
+    #initialize main row with urs vars
     for i in range(num_vars):
-        mainRow.append("x" + str(i+1))
+        if i in unrestricted_indices:
+            mainRow.append("x" + str(i + 1)+"+")
+            mainRow.append("x" + str(i + 1)+"-")
+        else:
+            mainRow.append("x" + str(i + 1))
+
 
     # Add original decision variable labels
     # if unrestricted_indices:
@@ -167,6 +180,7 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
     # else:
     #     for i in range(num_vars):
     #         mainRow.append("x" + str(i+1))
+
     deviation_vars = []
     Zs = []
     for i in range(len(RHS_G)):
@@ -177,12 +191,14 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
             basic_var.append(deviation_vars[i*2])
         else:
             basic_var.append(deviation_vars[i*2+1])
-        
+
     mainRow.extend(deviation_vars)
+    print(mainRow)
+
     artificial_vars = []
     for i, constraint_type in enumerate(constraint_types):
         if constraint_type == ">=":
-            mainRow.append("e" + str(j)) 
+            mainRow.append("e" + str(j))
             num_slack+=1
             j += 1
         if constraint_type == ">=" or constraint_type == "=":
@@ -191,7 +207,7 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
         else:
             num_slack+=1
             basic_var.append("s" + str(i + 1))
-            mainRow.append("s" + str(i + 1))    
+            mainRow.append("s" + str(i + 1))
 
     if len(artificial_vars) > 0:
         mainRow.extend(artificial_vars)
@@ -205,40 +221,68 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
 
     tableau[len(RHS_G):-len(Zs) , -1] = RHS_A
     tableau[:len(RHS_G) , -1] = RHS_G
-    tableau[len(RHS_G):-len(Zs) , :num_vars] = A
-    tableau[:len(RHS_G) , :num_vars] = G
+
+    #add A and G to the Tablue
+    num_of_urs_gone = 0
+    for i in range(len(G) + len(A)):
+        num_of_urs_gone = 0
+        for j in range(num_vars + len(unrestricted_indices)):
+            if i < len(G):
+                if mainRow[j][-1] == "-":
+                    if abs(tableau[i][j - 1]) > 1e-10:
+                        tableau[i, j] = -tableau[i][j - 1]
+                    num_of_urs_gone += 1
+                else:
+                    tableau[i, j] = G[i][j - num_of_urs_gone]
+            else:
+                if mainRow[j][-1] == "-":
+                    if abs(tableau[i][j - 1]) > 1e-10:
+                        tableau[i, j] = -tableau[i][j - 1]
+                    num_of_urs_gone += 1
+                else:
+                    tableau[i, j] = A[i - len(G)][j - num_of_urs_gone]
+
+    # tableau[len(RHS_G):-len(Zs) , :num_vars] = A
+    # tableau[:len(RHS_G) , :num_vars] = G
+
+    # number_of_urs = 0
+    # for i in range(tableau[0]):
+    #     if mainRow[i] :
     i=0
     for j in range (len(RHS_G)):
         tableau[len(RHS_G)+len(RHS_A)+i , mainRow.index(basic_var[j] )] = (-priorities[i])
         i+=1
-    j = num_vars
+    j = num_vars + len(unrestricted_indices)
     for i in range (len(RHS_G)):
         tableau[i, j] = 1
         tableau[i, j+1] = -1
         j= j+2
-    
+
     for i in range(len(RHS_G), len(RHS_G)+len(RHS_A), 1):
 
         j=0
         for k in range(num_vars+len(RHS_G)*2, num_cols-1, 1):
-            if j < num_constraints:  
+            if j < num_constraints:
                 if constraint_types[j] == ">=":
-                    tableau[i, start_slack] = -1
-                    tableau[i, start_artificial] = 1
+                    tableau[i, start_slack + len(unrestricted_indices)] = -1
+                    tableau[i, start_artificial+ len(unrestricted_indices)] = 1
                     start_artificial += 1
                     start_slack += 1
                 elif constraint_types[j] == "=":
-                    tableau[ i, start_artificial ] = 1
+                    tableau[ i, start_artificial + len(unrestricted_indices)] = 1
                     start_artificial += 1
                 else:
-                    tableau[i, start_slack] = 1
+                    tableau[i, start_slack+ len(unrestricted_indices)] = 1
                     start_slack += 1
                 j += 1
 
     new_tableau = tableau.copy()
+    print(tableau)
     make_vars_zeros_Linearly(tableau,mainRow ,basic_var)
     isDone = []
     iterations, solution,  new_basic_var , isDone  = __excute_simplex(tableau, basic_var.copy() ,mainRow,artificial_vars, 2,0, priorities)
+
+    print(solution)
 
     iterations = [new_tableau] + iterations
 
@@ -264,37 +308,54 @@ def goal_method( num_vars_original , A, RHS_A, G, RHS_G, constraint_types, Goal_
     #     solution_array = np.array([original_solution.get("x" + str(i+1), 0) for i in range(num_vars_original)])
     # else:
     #     solution_array = np.array(list(solution.values())[:num_vars])
+    # delete all unrestricted and all without objective function in the solution
+
+    #handle delete all unrestricted
+    for i in range(len(solution)):
+        if "x" + str(i + 1) + "-" in solution:
+            solution["x" + str(i + 1) + "+"] = solution["x" + str(i + 1) + "+"] - solution["x" + str(i + 1) + "-"]
+            del solution["x" + str(i + 1) + "-"]
+
 
     sol_array = np.array(list(solution.values())[:num_vars_original])
     return sol_array, iterations, mainRow, basic_var , isDone
 
 
 # Example usage
-# # # c = np.array([-1,-5])  # Objective function coefficients
-# A = np.array([[15,30]])  # Constraint coefficients
-# G = np.array([[200,0],[100,400] , [0,250]]) # Goals coefficients
-# RHS_A = np.array([150])  # RHS of constraints
-# RHS_G = np.array([1000,1200,800]) # RHS of goals
-# constraints_type = ['<=']  # Constraint types
-# goals_type = ['>=','>=','>='] # Goals types
-# priorites = [1,2,1]
-# # # isMax = 0  # 0 for minimization, 1 for maximization
-# variables_types =np.array(["Non-negative","Non-negative"])
-# np.set_printoptions(precision=3, suppress=True)
-# solution, iterations,mainRow,basic_var , isDone=  goal_method(2 ,A,RHS_A,G,RHS_G, constraints_type, goals_type,variables_types, priorites)
-# for i in range( len(isDone)):
-#     if isDone[i] == True:
-#         print("Goal", i + 1, "is done")
-#     else:
-#         print("Goal", i + 1, "is not done")
-#
-# print("Optimal solution:", solution)
-#
-# for i, iteration in enumerate(iterations):
-#       # Only print tableaus, not entering/leaving vars
-#         print(f"Iteration {i}:")
-#         print(iteration)
-#         print()
-#
-# print("Column headers:", mainRow)
-# print("Basic variables:", basic_var)
+# c = np.array([-1,-5])  # Objective function coefficients
+# Example with URS variable
+A = np.array([[15, 30]])  # Constraint coefficients (15x1 + 30x2 <= 150)
+G = np.array([[200, 0], [100, 400], [0, 250]])  # Goals coefficients
+RHS_A = np.array([150])  # RHS of constraints
+RHS_G = np.array([1000, 1200, 800])  # RHS of goals
+constraints_type = ['<=']  # Constraint types
+goals_type = ['>=', '>=', '>=']  # Goals types
+priorities = [1, 2, 1]  # Priorities for goals
+variables_types = ["Non-negative", "Non-negative"]  # x1 is non-negative, x2 is URS
+np.set_printoptions(precision=3, suppress=True)
+
+solution, iterations, mainRow, basic_var, isDone = goal_method(
+    2, A, RHS_A, G, RHS_G, constraints_type, goals_type, variables_types, priorities
+)
+
+# Print results
+for i in range(len(isDone)):
+    if isDone[i]:
+        print(f"Goal {i+1} is achieved")
+    else:
+        print(f"Goal {i+1} is not achieved")
+
+print("\nOptimal solution:", solution)
+print("\nBasic variables:", basic_var)
+print("\nMain row variables:", mainRow)
+
+# Print iterations
+for i, iteration in enumerate(iterations):
+    if isinstance(iteration, list):  # Skip entering/leaving var info
+        continue
+    print(f"\nIteration {i}:")
+    print(iteration)
+
+print("Column headers:", mainRow)
+print("Basic variables:", basic_var)
+print("Solution", solution)
